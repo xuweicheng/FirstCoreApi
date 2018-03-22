30d0ef5e01b443fd436d20dc89b712a983cbd885
xuweicheng <weicheng.xu@gmail.com>
2018-03-22 16:51:50 -0700

    initial project

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FirstCoreApi.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        //[HttpGet("api/cities")]
+        [HttpGet()]
+        public IActionResult getCities()
+        {
+            return new JsonResult(new List<object> {
+                new { id = 1, name ="Dalian" },
+                new { id = 2, name ="Vancouver" },
+            });
+        }
+
+    }
+}
