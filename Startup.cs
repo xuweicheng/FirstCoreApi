d3a1c83e4b422ffb134dc18aa6c69c2e27ac057b
xuweicheng <weicheng.xu@gmail.com>
2018-03-23 09:51:47 -0700

    add json serialize settings

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace FirstCoreApi
 {
@@ -12,6 +13,17 @@ namespace FirstCoreApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // turn of camel case json serialize strategy
+            //services.AddMvc()
+            //    .AddJsonOptions(o => {
+            //        if(o.SerializerSettings.ContractResolver != null)
+            //        {
+            //            var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+            //            castedResolver.NamingStrategy = null;
+            //        }
+            //    });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
